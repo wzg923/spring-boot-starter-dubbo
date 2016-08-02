04a57a965561e94ce16e8eb410412d3892e3d200
还是相信爱过 <191287278@qq.com>
2016-08-02 17:35:42 +0800

    更改 属性名

--- src/main/java/com/alibaba/boot/dubbo/DubboAutoConfiguration.java
+++ src/main/java/com/alibaba/boot/dubbo/DubboAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.alibaba.boot.dubbo;
 
 import com.alibaba.dubbo.config.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
